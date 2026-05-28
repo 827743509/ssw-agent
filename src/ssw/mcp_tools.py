1eede7550722476e17bf8cd67f3302cc704724e1
saltsodawater <827743509@qq.com>
2026-05-28 23:27:15 +0800

    mcp工具添加缓存

--- src/ssw/mcp_tools.py
+++ src/ssw/mcp_tools.py
@@ -1,4 +1,5 @@
 import asyncio
+from functools import lru_cache
 from typing import Any
 
 from langchain_mcp_adapters.client import MultiServerMCPClient
@@ -9,7 +10,7 @@ TRAVEL_MCP_CONNECTIONS = {
         "url": "http://127.0.0.1:8080/ssw-mcp-server/mcp",
     },
 }
-
+@lru_cache(maxsize=20)
 def _load_mcp_tools_by_name(name:str ) -> list[Any]:
     client = MultiServerMCPClient({name: TRAVEL_MCP_CONNECTIONS[name]})
     return asyncio.run(client.get_tools())
